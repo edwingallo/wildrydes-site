3cb33ebf4108cb1c23474d4a4450f2baa9fd2947
egallo <eddyc_22@hotmail.com>
2025-06-24 10:07:37 -0500

    auntenticador

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_diwAED1nX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '36f8323rgtd5m6fl9gno5gppc2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_EYm2nz2dI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14h4lpke8i38umrufdoiq7fcia', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://741g024jac.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
